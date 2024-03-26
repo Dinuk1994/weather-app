2750417f6c87a0e4dc3923b6c29af377495bf356
Dinuk1994 <ddt94119@gmail.com>
2024-03-26 15:32:55 +0530

    Implementing news section

---
+++ src/Contents/News/News.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../News/News.css'
+
+const News = () => {
+  return (
+    <div className='news mt-5'>News</div>
+  )
+}
+
+export default News
--- src/MainPage/Main.tsx
+++ src/MainPage/Main.tsx
@@ -3,6 +3,7 @@ import '../MainPage/Main.css';
 
 import Icon from '../assets/Photos/47314_weather_icon.png';
 import Detail from '../Contents/Detail/Detail';
+import News from '../Contents/News/News';
 
 const Main = () => {
     return (
@@ -12,8 +13,13 @@ const Main = () => {
                     <img src={Icon} alt="" className='icon' />
                     <label htmlFor="">Real Weather</label>
                 </div>
-                <div className='div-content'>
-                    <Detail/>
+
+                <div className='div-content d-flex justify-content-center'>
+                    <Detail />            
+                </div>
+
+                <div className=''>
+
                 </div>
             </div>
         </div>
